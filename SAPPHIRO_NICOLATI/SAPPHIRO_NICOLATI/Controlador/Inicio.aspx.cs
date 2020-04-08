4e8f0a2f1df3fc335c6bb109b85b385455e3822f
jnnieto <48180657+jnnieto@users.noreply.github.com>
2020-04-07 20:38:25 -0500

    web form inicio

--- SAPPHIRO_NICOLATI/SAPPHIRO_NICOLATI/Controlador/Inicio.aspx.cs
+++ SAPPHIRO_NICOLATI/SAPPHIRO_NICOLATI/Controlador/Inicio.aspx.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class Vista_PaginaInicial : System.Web.UI.MasterPage
+public partial class Vista_Inicio : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
